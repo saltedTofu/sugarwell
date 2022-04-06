cfa1a18af22e263ec392a5fb4584fbe9af70cb1c
Tyler Dansby <tdansby7@gmail.com>
2022-04-05 20:35:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form style={styles}>
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text"></input>
+      </form>
     </div>
   );
+}
+
+const styles = {
+
 }
 
 export default App;
