18788ede70b01234570b9dd388fde8acfec90603
Tyler Dansby <tdansby7@gmail.com>
2022-04-05 20:54:16 -0500

    form

--- src/App.js
+++ src/App.js
@@ -3,36 +3,14 @@ import './App.css';
 
 function App() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let myForm = document.getElementById("contactForm");
-    let formData = new FormData(myForm);
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-  };
-
   return (
     <div className="App">
-      <form id ="contactForm" name="contact" method="POST" data-netlify="true" onSubmit={()=>handleSubmit()}>
+      <form name="contact" netlify>
         <p>
-          <label>Your Name: <input type="text" name="name" /></label>
+          <label>Name <input type="text" name="name" /></label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>Email <input type="email" name="email" /></label>
         </p>
         <p>
           <button type="submit">Send</button>
