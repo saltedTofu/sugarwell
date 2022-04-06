b3fac5ff6881e1abb44f8e4bc215b162c02f917a
Tyler Dansby <tdansby7@gmail.com>
2022-04-05 20:51:02 -0500

    form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById("contactForm");
+    let formData = new FormData(myForm);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div className="App">
-      <form name="waterForm" method="POST" data-netlify="true">
-        <label htmlFor="name">Name:</label>
-        <input id="name" type="text"></input>
-        <button type="submit">Send</button>
+      <form id ="contactForm" name="contact" method="POST" data-netlify="true" onSubmit={()=>handleSubmit()}>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
     </div>
   );
